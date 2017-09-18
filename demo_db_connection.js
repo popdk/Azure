c5f78ed1d9260a1471dd5e356a122e9ad10bd313
Akshat <akshat.desai@einfochips.com>
2017-09-18 18:12:22 +0530

    Newest Commit

---
+++ demo_db_connection.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+console.log("Its me again");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
